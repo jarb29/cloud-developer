2d01c8f7d480a1fc753ac1fee389ba956c551dc8
jarb29 <jarb29@gmail.com>
2020-12-07 13:36:40 -0300

    type: server body: adding auth

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -3,10 +3,11 @@ import { config } from './config/config';
 
 const c = config.dev;
 
-//Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.credentials = credentials;
+console.log(c, "para ver que es SSSSSSSSSSSSSSSSSSSSSSSSS")
 
+//Configure AWS
+var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
+AWS.config.credentials = credentials;
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
   region: c.aws_region,
